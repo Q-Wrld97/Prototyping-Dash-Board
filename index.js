120bece12004ac1db3608e78ae4c9e799e63b604
Q-wrld97 <dough_boy97@hotmail.com>
2023-02-11 17:46:33 -0500

    Prototype

---
+++ index.js
@@ -0,0 +1,6 @@
+function yesnoCheck() {
+    if (document.getElementById('customRadio3').checked) {
+        document.getElementById('ifYes').style.display = 'block';
+    }
+    else document.getElementById('ifNo').style.display = 'none';
+}
