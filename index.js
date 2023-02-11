800d4e69a5d935b185413183539b52314a48b4f2
Q-wrld97 <dough_boy97@hotmail.com>
2023-02-11 18:18:54 -0500

    JS for Custom appear or not appear

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ function yesnoCheck() {
     if (document.getElementById('customRadio3').checked) {
         document.getElementById('ifYes').style.display = 'block';
     }
-    else document.getElementById('ifNo').style.display = 'none';
+    if (document.getElementById('customRadio2').checked) {
+      document.getElementById('ifYes').style.display = 'none';
+    }
+    if (document.getElementById('customRadio1').checked) {
+    document.getElementById('ifYes').style.display = 'none';
+    }
 }
