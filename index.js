e1a5eb744007e1bfab6490f46904f08aa2916a66
mSakib20 <msakib2022@fau.edu>
2023-02-18 21:35:03 -0500

    login screen + database collab

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyACtLJPVmbrm-VTam80pmNC6ev_C9mVTiA",
+  authDomain: "testing-320ed.firebaseapp.com",
+  projectId: "testing-320ed",
+  storageBucket: "testing-320ed.appspot.com",
+  messagingSenderId: "759480422005",
+  appId: "1:759480422005:web:ed1e51cd3d09550041b23c"
+
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth()
+const database = firebase.database()
+const db = firebase.firestore()
+
+// Below is for dashboard functionality
+
 function yesnoCheck() {
     if (document.getElementById('customRadio3').checked) {
         document.getElementById('ifYes').style.display = 'block';
@@ -121,6 +139,9 @@ function GradeCategory() {
       document.getElementById("inlineCheckbox8").checked = false;
     }
   }
+
+
+
 
 
 
---
+++ loginandsignup.js
@@ -0,0 +1,89 @@
+// Set up our register function
+function register() {
+  // Get all our input fields
+  email = document.getElementsByName('email')[0].value
+  password = document.getElementsByName('password')[0].value
+
+  // Validate input fields
+  if (validate_email(email) == false || validate_password(password) == false) {
+    alert('Bad email or Password')
+    return
+    // Don't continue running the code
+  }
+ 
+  // Move on with Auth
+  auth.createUserWithEmailAndPassword(email, password)
+  .then((res) => {
+    db.collection('users').doc(res.user.uid).set({
+     email:email
+    })  
+    alert('User Created!!')
+  })
+  .catch((error) => {
+    // Firebase will use this to alert of its errors
+    var error_code = error.code
+    var error_message = error.message
+
+    alert(error_message)
+  })
+}
+
+
+// Set up our login function
+function login () {
+  // Get all our input fields
+  email = document.getElementsByName('email')[0].value
+  password = document.getElementsByName('password')[0].value
+
+  // Validate input fields
+  if (validate_email(email) == false || validate_password(password) == false) {
+    alert('Email or Password is Wrong!')
+    return
+    // Don't continue running the code
+  }
+
+  auth.signInWithEmailAndPassword(email, password)
+  .then((res) => {{
+    db.collection('users').doc(res.user.uid).update({
+      loginTime:firebase.firestore.FieldValue.serverTimestamp(),
+    })
+    .then(() => {
+      window.location = 'test.html';})
+    // done
+    alert('User Logged In!!')
+     
+    
+
+  }})
+  .catch((error) => {
+    // Firebase will use this to alert of its errors
+    var error_code = error.code
+    var error_message = error.message
+
+    alert(error_message)
+  })
+  
+}
+
+
+
+// Validate Functions
+function validate_email(email) {
+  expression = /^[^@]+@\w+(\.\w+)+\w$/
+  if (expression.test(email) == true) {
+    // Email is good
+    return true
+  } else {
+    // Email is not good
+    return false
+  }
+}
+
+function validate_password(password) {
+  // Firebase only accepts lengths greater than 6
+  if (password < 6) {
+    return false
+  } else {
+    return true
+  }
+}
